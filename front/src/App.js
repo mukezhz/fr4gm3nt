c1626b4c88b4ea47675dc77315ac3b9a2ba88e4b
twobits <avash700@gmail.com>
2021-12-17 12:15:16 +0545

    login and register pages

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,28 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <div className="text-4xl font-extrabold">
-        hi
-      </div>
+      <BrowserRouter>
+
+        {/* navigation */}
+        <Nav />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+
+        {/* footer */}
+        <Footer />
+
+      </BrowserRouter>
     </>
   );
 }
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ["Spartan", "sans-serif"],
+        // subheading: ["Libre Baskerville", "serif"]
+        subheading: ["Fanwood Text","serif"],
+        card: ["Montserrat", "sans-serif"]
+       },
+    },
   },
   plugins: [],
 }
